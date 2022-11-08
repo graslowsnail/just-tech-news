838d00986ba86d2265c46578415446bfee93e5fa
graslowsnail <graslowsnail@gmail.,com>
2022-11-08 14:05:26 -0600

    .findAll reurns everything but excludes passowrd

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+//import the sequlize constructor from the library
+const Sequelize = require("sequelize");
+require("dotenv").config();
+
+//create connection to our database, pass in your mysql infromation for username and password
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+
+  {
+    socketPath: "/tmp/mysql.sock",
+    host: "localhost",
+    dialect: "mysql",
+    port: 3306,
+  }
+);
+
+module.exports = sequelize;
---
+++ routes/api/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+
+const userRouter = require('./user-routes.js');
+
+router.use('/users', userRouter);
+
+module.exports = router;
+
