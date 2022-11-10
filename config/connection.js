33345da5e9039e06912448fd0719e2fd56755265
graslowsnail <graslowsnail@gmail.,com>
2022-11-09 18:14:31 -0600

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,38 @@
-//import the sequlize constructor from the library
+// import the sequlize constructor from the library
 const Sequelize = require("sequelize");
 
 require("dotenv").config();
 
-//create connection to our database, pass in your mysql infromation for username and password
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+// //create connection to our database, pass in your mysql infromation for username and password
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
 
-  {
-    dialectOptions: {
-      socketPath: "/tmp/mysql.sock",
-    },
-    host: "localhost",
-    dialect: "mysql",
-    port: 3306,
-  }
-);
+//   {
+//     dialectOptions: {
+//       socketPath: "/tmp/mysql.sock",
+//     },
+//     host: "localhost",
+//     dialect: "mysql",
+//     port: 3306,
+//   }
+// );
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: "localhost",
+      dialect: "mysql",
+      port: 3306,
+    }
+  );
+}
 
 module.exports = sequelize;
