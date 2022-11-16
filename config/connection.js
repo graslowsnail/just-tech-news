e2355669143b68da81a61b4daf0e4880b5691b0d
graslowsnail <graslowsnail@gmail.,com>
2022-11-16 16:24:29 -0600

    linked models to html. Post now show up on the main page

--- config/connection.js
+++ config/connection.js
@@ -4,35 +4,35 @@ const Sequelize = require("sequelize");
 require("dotenv").config();
 
 // //create connection to our database, pass in your mysql infromation for username and password
-// const sequelize = new Sequelize(
-//   process.env.DB_NAME,
-//   process.env.DB_USER,
-//   process.env.DB_PASSWORD,
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
 
-//   {
-//     dialectOptions: {
-//       socketPath: "/tmp/mysql.sock",
-//     },
-//     host: "localhost",
-//     dialect: "mysql",
-//     port: 3306,
-//   }
-// );
-let sequelize;
+  {
+    dialectOptions: {
+      socketPath: "/tmp/mysql.sock",
+    },
+    host: "localhost",
+    dialect: "mysql",
+    port: 3306,
+  }
+);
+// let sequelize;
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: "localhost",
-      dialect: "mysql",
-      port: 3306,
-    }
-  );
-}
+// if (process.env.JAWSDB_URL) {
+//   sequelize = new Sequelize(process.env.JAWSDB_URL);
+// } else {
+//   sequelize = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASSWORD,
+//     {
+//       host: "localhost",
+//       dialect: "mysql",
+//       port: 3306,
+//     }
+//   );
+// }
 
 module.exports = sequelize;
--- server.js
+++ server.js
@@ -1,12 +1,22 @@
+const path = require('path');
 const express = require("express");
-const routes = require("./routes");
+const routes = require("./controllers");
 const sequelize = require("./config/connection.js");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//sets up handlebars.js
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+//middlewear
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //turn on routes
 app.use(routes);
